f5056aaaae1bf0a5152a6e7e3f27a2ec5130d4f2
malsoft <github@malsoft.com>
2023-06-11 16:38:52 +0100

    Initial definition for the ClaimContext using the DDL models supplied

---
+++ ClaimsApi.Data/Context/ClaimContext.cs
@@ -0,0 +1,42 @@
+﻿using ClaimsApi.Data.Entities;
+using ClaimsApi.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClaimsApi.Data.Context
+{
+	public class ClaimContext :
+		DbContext,
+		IClaimContext
+	{
+		#region Ctor
+
+		public ClaimContext(DbContextOptions<ClaimContext> options) :
+			base(options)
+		{
+		}
+
+		#endregion Ctor
+
+		#region overrides
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+
+		#endregion overrides
+
+		#region IClaimContext implementation
+
+		/// <inheritdoc />
+		public DbSet<Claim>? Claims { get; set; }
+
+		/// <inheritdoc />
+		public DbSet<ClaimType>? ClaimTypes { get; set; }
+
+		/// <inheritdoc />
+		public DbSet<Company>? Companies { get; set; }
+
+		#endregion IClaimContext implementation
+	}
+}
