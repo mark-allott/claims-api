f02e79c26b05045749d2a78eefd548d1ecc09149
malsoft <github@malsoft.com>
2023-06-12 09:58:22 +0100

    Add response models for API endpoints

---
+++ claims-api/Models/AbstractResponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ClaimsApi.Models
+{
+	public abstract class AbstractResponseModel<T> where T : class
+	{
+		protected AbstractResponseModel(T data)
+		{
+			Data = data;
+		}
+
+		/// <summary>
+		/// Indicates success or failure of the operation
+		/// </summary>
+		public bool Success { get; set; }
+
+		/// <summary>
+		/// Provides a message suitable for the state of <see cref="Success"/>
+		/// </summary>
+		public string Message { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Placeholder for the data payload
+		/// </summary>
+		public T? Data { get; }
+	}
+}
---
+++ claims-api/Models/CompanyResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using ClaimsApi.Data.DTO;
+
+namespace ClaimsApi.Models
+{
+	/// <summary>
+	/// Strongly-typed response model for Company operations
+	/// </summary>
+	public class CompanyResponseModel :
+		AbstractResponseModel<CompanyDto>
+	{
+		public CompanyResponseModel(CompanyDto data) :
+			base(data)
+		{
+		}
+	}
+}
