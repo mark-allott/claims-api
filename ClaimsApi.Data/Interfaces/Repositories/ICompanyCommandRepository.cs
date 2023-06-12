0e35ccf31d167a11677dea388308aa52b9b6f2e7
malsoft <github@malsoft.com>
2023-06-12 10:00:07 +0100

    Add strongly-typed interfaces for repositories

---
+++ ClaimsApi.Data/Interfaces/Repositories/ICompanyCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClaimsApi.Data.Entities;
+
+namespace ClaimsApi.Data.Interfaces.Repositories
+{
+	/// <summary>
+	/// Strongly-typed version of the command repository for accessing Company data
+	/// </summary>
+	public interface ICompanyCommandRepository :
+		ICommandRepository<Company>
+	{
+	}
+}
---
+++ ClaimsApi.Data/Interfaces/Repositories/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using ClaimsApi.Data.Entities;
+
+namespace ClaimsApi.Data.Interfaces.Repositories
+{
+	/// <summary>
+	/// Strongly-typed version of the repository
+	/// </summary>
+	public interface ICompanyRepository :
+		IRepository<Company>,
+		ICompanyCommandRepository,
+		ICompanyQueryRepository
+	{
+	}
+}
