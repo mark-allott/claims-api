01039df85e54f137d08a8eb324c3fd56fc97ec2f
malsoft <github@malsoft.com>
2023-06-12 09:58:58 +0100

    Define interfaces for claim and company services

---
+++ ClaimsApi.Data/Interfaces/Services/IClaimService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClaimsApi.Data.DTO;
+using ClaimsApi.Data.Entities;
+
+namespace ClaimsApi.Data.Interfaces.Services
+{
+	internal interface IClaimService :
+		IAutoRegisterService
+	{
+		/// <summary>
+		/// Performs a search for a claim with the <see cref="Claim.Ucr"/> value matching <paramref name="ucr"/>
+		/// </summary>
+		/// <param name="ucr">The claim relating to <paramref name="ucr"/></param>
+		/// <returns>A claim object, if found, or null</returns>
+		ClaimDto? GetClaim(string ucr);
+
+		/// <summary>
+		/// Attempts to update the claim with the information supplied in <paramref name="claim"/>
+		/// </summary>
+		/// <param name="claim">Details of the claim to be updated</param>
+		/// <returns>A flag indicating success or failure</returns>
+		bool UpdateClaim(ClaimDto claim);
+	}
+}
---
+++ ClaimsApi.Data/Interfaces/Services/ICompanyService.cs
@@ -0,0 +1,23 @@
+﻿using ClaimsApi.Data.DTO;
+using ClaimsApi.Data.Entities;
+
+namespace ClaimsApi.Data.Interfaces.Services
+{
+	public interface ICompanyService :
+		IAutoRegisterService
+	{
+		/// <summary>
+		/// Performs a search for a company with the specified <paramref name="id"/>
+		/// </summary>
+		/// <param name="id">The id to search for (refers to the <see cref="Company.Id"/> column, not its PK identity)</param>
+		/// <returns>Details of the company, without any details of claims</returns>
+		CompanyDto? GetCompany(int id);
+
+		/// <summary>
+		/// Performs a search for a company with the specified <paramref name="id"/>
+		/// </summary>
+		/// <param name="id">The id to search for (refers to the <see cref="Company.Id"/> column, not its PK identity)</param>
+		/// <returns>Details of the company, with details of claims submitted</returns>
+		CompanyDto? GetCompanyClaims(int id);
+	}
+}
