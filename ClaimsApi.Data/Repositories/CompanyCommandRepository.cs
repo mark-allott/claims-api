a64113f3adf5dede2dc3e922176ddca34ef989de
malsoft <github@malsoft.com>
2023-06-12 10:07:39 +0100

    Add concrete implementations of repositories for Company

---
+++ ClaimsApi.Data/Repositories/CompanyCommandRepository.cs
@@ -0,0 +1,17 @@
+﻿using ClaimsApi.Data.Context;
+using ClaimsApi.Data.Entities;
+using ClaimsApi.Data.Interfaces.Repositories;
+
+namespace ClaimsApi.Data.Repositories
+{
+	public class CompanyCommandRepository :
+		AbstractCommandRepository<ClaimContext, Company>,
+		ICompanyCommandRepository
+
+	{
+		public CompanyCommandRepository(ClaimContext context) :
+			base(context)
+		{
+		}
+	}
+}
---
+++ ClaimsApi.Data/Repositories/CompanyQueryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ClaimsApi.Data.Context;
+using ClaimsApi.Data.Entities;
+using ClaimsApi.Data.Interfaces.Repositories;
+
+namespace ClaimsApi.Data.Repositories
+{
+	public class CompanyQueryRepository :
+		AbstractQueryRepository<ClaimContext, Company>,
+		ICompanyQueryRepository
+	{
+		public CompanyQueryRepository(ClaimContext context) :
+			base(context)
+		{
+		}
+	}
+}
