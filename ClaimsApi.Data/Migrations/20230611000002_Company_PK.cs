86f996ec7a5ef8be56456eede8a5eb337da8bc20
malsoft <github@malsoft.com>
2023-06-12 08:14:37 +0100

    Add migration for new identity column for Company

---
+++ ClaimsApi.Data/Migrations/20230611000002_Company_PK.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClaimsApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Company_PK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Identity",
+                table: "Company",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identity",
+                table: "Company");
+        }
+    }
+}
