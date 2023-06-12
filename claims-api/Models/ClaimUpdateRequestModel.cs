107a7a7839b3d81e2fc1d7bc4232137a39611072
malsoft <github@malsoft.com>
2023-06-12 12:03:26 +0100

    Add a request model for performing claim updates

---
+++ claims-api/Models/ClaimUpdateRequestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ClaimsApi.Models
+{
+	public class ClaimUpdateRequestModel
+	{
+		public string Ucr { get; set; } = string.Empty;
+		public DateTime? ClaimDate { get; set; }
+
+		public DateTime? LossDate { get; set; }
+
+		public string? AssuredName { get; set; }
+
+		public decimal? IncurredLoss { get; set; }
+
+		public bool Closed { get; set; }
+	}
+}
