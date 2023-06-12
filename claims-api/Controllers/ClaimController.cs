2b803a9cc379c5a1401a741eb7642e6e2ba2ca76
malsoft <github@malsoft.com>
2023-06-12 13:25:58 +0100

    Implement claim controller

--- claims-api/Controllers/ClaimController.cs
+++ claims-api/Controllers/ClaimController.cs
@@ -1,11 +1,85 @@
-﻿namespace ClaimsApi.Controllers
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+using ClaimsApi.Data.Interfaces.Services;
+using ClaimsApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClaimsApi.Controllers
 {
 	public class ClaimController :
 		AbstractApiController
 	{
-		public ClaimController(ILoggerFactory loggerFactory) :
+		#region Ctor
+
+		private readonly IClaimService _claimService;
+		private readonly Regex _ucrPattern = new Regex("^\\w{3}-\\d{8}", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+		public ClaimController(ILoggerFactory loggerFactory, IClaimService claimService) :
 			base(loggerFactory)
 		{
+			_claimService = claimService;
+		}
+
+		#endregion Ctor
+
+		[HttpGet]
+		[Route("{ucr:required}")]
+		[ProducesDefaultResponseType(typeof(ClaimResponseModel))]
+		public IActionResult GetClaim([FromRoute] string ucr)
+		{
+			try
+			{
+				var claim = _claimService.GetClaim(ucr);
+
+#if DEBUG
+				if (claim is null && _ucrPattern.IsMatch(ucr.Trim()))
+					claim = _claimService.CreateTestClaim(ucr);
+#endif
+				var isSuccessful = claim is not null;
+
+				var result = new ClaimResponseModel(claim)
+				{
+					Success = isSuccessful,
+					Message = isSuccessful
+						? "Success"
+						: $"No result for {ucr}"
+				};
+
+				return new JsonResult(result);
+			}
+			catch (Exception e)
+			{
+				Logger.LogError(e, $"Error when executing {nameof(GetClaim)} with {nameof(ucr)}='{ucr}'");
+			}
+
+			return BadRequest();
+		}
+
+		[HttpPost]
+		[Route("update")]
+		[ProducesDefaultResponseType(typeof(ClaimResponseModel))]
+		public IActionResult UpdateClaim([FromBody] ClaimUpdateRequestModel claim)
+		{
+			try
+			{
+				var updateResult = _claimService.UpdateClaim(claim.Ucr, claim.ClaimDate, claim.LossDate, claim.AssuredName,
+					claim.IncurredLoss, claim.Closed);
+
+				var result = new ClaimResponseModel(null)
+				{
+					Success = updateResult,
+					Message = updateResult
+						? "Success"
+						: "Could not update claim"
+				};
+
+				return new JsonResult(result);
+			}
+			catch (Exception e)
+			{
+				Logger.LogError(e, $"Error when executing {nameof(UpdateClaim)} with payload: {JsonSerializer.Serialize(claim)}");
+				return new JsonResult(new ClaimResponseModel(null) { Success = false, Message = e.Message });
+			}
 		}
 	}
 }
